b935bd30f3a9c3e4f50fece665fbcf851d3bdab1
HaNeul Jung <jhn153228@gmail.com>
2024-02-28 20:52:54 +0900

    APIView

--- instagram/views.py
+++ instagram/views.py
@@ -1,11 +1,33 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
 
-class PublicPostListAipView(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# 제네릭 방식
+# class PublicPostListApiView(generics.ListAPIView):
+#     queryset = Post.objects.filter(is_public=True)
+#     serializer_class = PostSerializer
+
+# APIView 클래스 방식
+# class PublicPostListAPIView(APIView):
+#     def get(self, request):
+#         qs = Post.objects.filter(is_public=True)
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+#
+# public_post_list = PublicPostListAPIView.as_view()
+
+''' APIView 함수방식
+간단하게 한개의 뷰만 만들때 사용하면 좋을듯 하다
+'''
+@api_view(['GET'])
+def public_post_list(request):
+    qs = Post.objects.filter(is_public=True)
+    serializer = PostSerializer(qs, many=True)
+    return Response(serializer.data)
 
 
 class PostViewSet(ModelViewSet):
