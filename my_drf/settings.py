9c4fb949d106a0be3e84e70075f2da62984a9aa0
HaNeul Jung <jhn153228@gmail.com>
2024-03-03 05:11:54 +0900

    Throttling - 유저/IP별 API 최대 호출 횟수 제한

--- my_drf/settings.py
+++ my_drf/settings.py
@@ -125,7 +125,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'PAGE_SIZE': 2,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATE': {
+        'user': '3/day',
+    }
 }
